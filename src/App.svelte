b290e2197caf6db7ed4ef33480c0c1584ff88796
Vadim <tolkott35@gmail.com>
2025-06-01 14:05:22 +0500

    Подправил анимации

--- src/App.svelte
+++ src/App.svelte
@@ -4,28 +4,14 @@ import SlideButton from './SlideButton.svelte'
 import Dot   	   from './Dot.svelte'
 
 	let slides = ["1", "2", "3", "4"]; // Массив наших слайдов 
-	let active = 1; 			  // Указывает какой номер слайда будет отображаться первым
-	let blocks = []; 			  // Ссылка на DOM элемент массива slides
-    let contentDiv;  			  // Ссыдка на DOM элемент слайдера
-	const SLIDE_INDENTATION = 100 // Наша константа отступа px от слайда
+	let active = 1; 			       // Указывает какой номер слайда будет отображаться первым
 
-    $: if(slides.length && contentDiv) {
-        const slideWidth = blocks[0].offsetWidth; // возможно стоит переименовать blocks в slideLink или slideDOM
-        const contentWidth = contentDiv.offsetWidth;
-        const centerX = (contentWidth / 2) - (slideWidth / 2);
-
-        for (let i = 0; i < blocks.length; i++) {
-            const offsetFromActive = (i - active) * SLIDE_INDENTATION;
-            const x = centerX + offsetFromActive;
-            blocks[i].style.transform = `translate(${x}px, 0px)`;
-        }
-    }
 	function increment() {
-		const newActive = (active + 1) % blocks.length;
+		const newActive = (active + 1) % slides.length;
         updateActive(newActive);
 	}
 	function decrement() {
-		const newActive = (active - 1 + blocks.length) % blocks.length;
+		const newActive = (active - 1 + slides.length) % slides.length;
         updateActive(newActive);
 	}
 	function updateActive(newActive) {
@@ -36,7 +22,7 @@ import Dot   	   from './Dot.svelte'
 <main>
 	<div class="flex items-center justify-center mt-40 mb-10 gap-4">
 		<SlideButton on:click={decrement} content="-"/>
-		<Slide slides={slides} active={active} bind:blocks bind:contentDiv />
+		<Slide slides={slides} active={active} />
 		<SlideButton on:click={increment} content="+"/>
 	</div>
 	<Dot dots={slides} active={active}/> 
--- src/Slide.svelte
+++ src/Slide.svelte
@@ -1,13 +1,33 @@
 <script>
+    import { fade } from 'svelte/transition';
+
     export let slides = [];
     export let active;
-    export let blocks = [];
-    export let contentDiv;
+    let blocks = [];              // Массив с DOM slide
+    let contentDiv;               // Наш контейнер
+    const SLIDE_INDENTATION = 100 // Наша константа отступа px от слайда
 
+    $: if(slides.length && contentDiv) {
+        const slideWidth = blocks[0].offsetWidth; // возможно стоит переименовать blocks в slideLink или slideDOM
+        const contentWidth = contentDiv.offsetWidth;
+        const centerX = (contentWidth / 2) - (slideWidth / 2);
+
+        for (let i = 0; i < blocks.length; i++) {
+            const offsetFromActive = (i - active) * SLIDE_INDENTATION;
+            const x = centerX + offsetFromActive;
+            blocks[i].style.transform = `translate(${x}px, 0px)`;
+        }
+    }
 </script>
 
 <main class="flex justify-between gap-4 relative overflow-hidden" bind:this={contentDiv}>
-    {#each slides as slide, i}
-        <div bind:this={blocks[i]} class="slide {i === active ? 'active' : ''}">{slide}</div>
-    {/each}
+  {#each slides as slide, i}
+    <div
+      transition:fade={{ delay: i * 200, duration: 2000 }}
+      bind:this={blocks[i]}
+      class="slide {i === active ? 'active' : ''}"
+    >
+      {slide}
+    </div>
+  {/each}
 </main>
